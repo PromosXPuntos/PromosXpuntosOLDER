d1401e928aeba458e89cac37ee0a9ce63d89d610
jorge Bonilla <jabonillab@unal.edu.co>
2015-11-05 08:37:04 -0500

    add test product

--- test/unit/CLASSES/StandardUserSpec.groovy
+++ test/unit/CLASSES/StandardUserSpec.groovy
@@ -2,19 +2,36 @@ package CLASSES
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @TestFor(StandardUser)
 class StandardUserSpec extends Specification {
 
     def setup() {
+        mockForConstraintsTests(StandardUser)
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    @Unroll("test StandardUser attribute gender: #gender, field: #field, valid: #valid ")
+    def "Testing Domain Class StandardUser - gender"() {
+        when:
+        def t_Product_gender = new Product(gender: gender, birthday: '31/12/99', telephone: 34554433, points: 1)
+        t_Product_gender.validate()
+
+        then:
+        t_Product_gender.errors.hasFieldErrors("gender") == false
+
+        where:
+        gender   | valid | field
+        null     | false | 'null'
+        'ok'     | false | 'algo'
+        'male'   | true  | 'male'
+        'female' | true  | 'female'
     }
+
 }
