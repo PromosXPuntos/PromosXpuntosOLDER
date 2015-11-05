1060328173a0defe0aef5942a0feb58d77667d94
jorge Bonilla <jabonillab@unal.edu.co>
2015-11-05 11:24:31 -0500

    add test product

--- test/unit/CLASSES/StandardUserSpec.groovy
+++ test/unit/CLASSES/StandardUserSpec.groovy
@@ -33,5 +33,21 @@ class StandardUserSpec extends Specification {
         'male'   | true  | 'male'
         'female' | true  | 'female'
     }
+    @Unroll("test StandardUser attribute telephone: #telephone, field: #field, valid: #valid ")
+    def "Testing Domain Class StandardUser - telephone"() {
+        when:
+        def t_Product_telephone = new Product(gender: 'male', birthday: '31/12/99', telephone: telephone, points: 1)
+        t_Product_telephone.validate()
+
+        then:
+        t_Product_telephone.errors.hasFieldErrors("telephone") == false
+
+        where:
+        telephone   | valid | field
+        null           | false | 'null'
+        123           | false | 'minRange'
+        1234567890123 | false | 'maxRange'
+        12345678 | true  | 'Range'
+    }
 
 }
